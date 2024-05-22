6e8a61da9e8f2b4e0d5d65cc6bbba8dd0b9c045e
KJ-Hyeon <bark052526@gmail.com>
2024-05-22 11:32:51 +0900

    선택정렬

--- app/src/main/java/com/sort/Selection.kt
+++ app/src/main/java/com/sort/Selection.kt
@@ -1,9 +1,22 @@
 package com.sort
 
-fun main() {
+import kotlin.math.min
 
+fun main() {
+    val array = intArrayOf(1,2,2,3,4,7,8,1,9,12,15,16,55,21,11)
+    println(selection(array).joinToString())
 }
 
-fun selection() {
-
+fun selection(arr: IntArray): IntArray {
+    var minIndex = 0
+    for (i in arr.indices) {
+        minIndex = i
+        for (j in i+1..arr.lastIndex) {
+            if (arr[minIndex] > arr[j]) minIndex = j
+        }
+        val temp = arr[i]
+        arr[i] = arr[minIndex]
+        arr[minIndex] = temp
+    }
+    return arr
 }
